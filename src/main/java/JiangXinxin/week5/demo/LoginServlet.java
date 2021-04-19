92c5f514e39b0a3bdd08d5d65fdc87d474762fee
JiangXinxin2019211001001208 <2758477002@>
2021-04-19 12:46:48 +0800

    7.HE week7 Commit

--- src/main/java/JiangXinxin/week5/demo/LoginServlet.java
+++ src/main/java/JiangXinxin/week5/demo/LoginServlet.java
@@ -40,7 +40,7 @@ public class LoginServlet extends HttpServlet {
             User user = (User) userDao.findByUsernamePassword(con,username,password);
             if (user!=null){
                 req.setAttribute("user",user);
-                req.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(req,resp);
+                req.getRequestDispatcher("WEB-INF/views/userinfo.jsp").forward(req,resp);
             }else {
                 req.setAttribute("massage","username or Password Error!!!");
                 req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req,resp);
